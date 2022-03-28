1fa018a941af901657191adac03281cbbf0b27e4
rainbowfilm1 <102548628+rainbowfilm1@users.noreply.github.com>
2022-03-29 00:32:09 +0530

    Update Stopwords.py

--- Stopwords.py
+++ Stopwords.py
@@ -1,23 +1,7 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
 input_str="NLTK is a leading platform for building Python programs to work with human language data."
-
 stop_words=set(stopwords.words('english'))
-
 tokens=word_tokenize(input_str)
-
 result=[i for i in tokens if not i in stop_words]
 print(result) 
-
-
-
-
-
-
-
-
-
-
-
-
